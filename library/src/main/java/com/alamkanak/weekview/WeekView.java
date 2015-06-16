f8a541a821283077398d095c3e49e419f1af3dbf
Laurent Meyer <laurent@moid.de>
2015-06-16 13:22:06 +0200

    Corrected the docs following @caske33 advices

--- library/src/main/java/com/alamkanak/weekview/WeekView.java
+++ library/src/main/java/com/alamkanak/weekview/WeekView.java
@@ -1468,8 +1468,10 @@ public class WeekView extends View {
     public interface MonthChangeListener {
         /**
          * Very important interface, it's the base to load events in the calendar.
+         * This method is called three times: once to load the previous month, once to load the next month and once to load the current month.
+         * <strong>That's why you can have three times the same event at the same place if you mess up with the configuration</strong>
          * @param newYear: year of the events required by the view.
-         * @param newMonth: month of the events required by the view.
+         * @param newMonth: month of the events required by the view <strong>1 based (not like JAVA API) --> January = 1 and December = 12</strong>.
          * @return a list of the events happening <strong>during the specified month</strong>.
          */
         public List<WeekViewEvent> onMonthChange(int newYear, int newMonth);
