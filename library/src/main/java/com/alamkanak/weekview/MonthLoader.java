1d37ab7b347606d1f23706979e1276ef6487ca2e
Samim Pezeshki <p.samim@gmail.com>
2015-12-31 23:36:32 +0330

    Support subclasses of WeekViewEvent

--- library/src/main/java/com/alamkanak/weekview/MonthLoader.java
+++ library/src/main/java/com/alamkanak/weekview/MonthLoader.java
@@ -17,7 +17,7 @@ public class MonthLoader implements WeekViewLoader {
     }
 
     @Override
-    public List<WeekViewEvent> onLoad(int periodIndex){
+    public List<? extends WeekViewEvent> onLoad(int periodIndex){
         return mOnMonthChangeListener.onMonthChange(periodIndex / 12, periodIndex % 12 + 1);
     }
 
@@ -34,10 +34,10 @@ public class MonthLoader implements WeekViewLoader {
          * Very important interface, it's the base to load events in the calendar.
          * This method is called three times: once to load the previous month, once to load the next month and once to load the current month.<br/>
          * <strong>That's why you can have three times the same event at the same place if you mess up with the configuration</strong>
-         * @param newYear: year of the events required by the view.
-         * @param newMonth: month of the events required by the view <br/><strong>1 based (not like JAVA API) --> January = 1 and December = 12</strong>.
+         * @param newYear : year of the events required by the view.
+         * @param newMonth : month of the events required by the view <br/><strong>1 based (not like JAVA API) --> January = 1 and December = 12</strong>.
          * @return a list of the events happening <strong>during the specified month</strong>.
          */
-        List<WeekViewEvent> onMonthChange(int newYear, int newMonth);
+        List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth);
     }
 }
