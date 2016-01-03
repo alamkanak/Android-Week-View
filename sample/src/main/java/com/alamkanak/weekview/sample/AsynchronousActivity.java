283e8dac494804446af351b5f727db523f2580b3
Alam Kanak <alam.kanak@gmail.com>
2016-01-03 18:35:34 +0600

    Update button texts

--- sample/src/main/java/com/alamkanak/weekview/sample/AsynchronousActivity.java
+++ sample/src/main/java/com/alamkanak/weekview/sample/AsynchronousActivity.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class AsynchronousActivity extends BaseActivity {
 
+    private List<WeekViewEvent> events;
+
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
         return null;
