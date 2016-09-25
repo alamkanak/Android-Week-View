8c15882e2dbe7ddd3e689d89e02dbecf83463309
Jesse Hoobergs <jesse@hoobergs.be>
2016-09-25 19:06:57 +0200

    DateTimeInterpreter to interface instead of abstract class

--- library/src/main/java/com/alamkanak/weekview/DateTimeInterpreter.java
+++ library/src/main/java/com/alamkanak/weekview/DateTimeInterpreter.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 /**
  * Created by Raquib on 1/6/2015.
  */
-public abstract class DateTimeInterpreter {
-    public abstract String interpretDate(Calendar date);
-    public abstract String interpretTime(int hour, int minutes);
+public interface DateTimeInterpreter {
+    public String interpretDate(Calendar date);
+    public String interpretTime(int hour, int minutes);
 }
