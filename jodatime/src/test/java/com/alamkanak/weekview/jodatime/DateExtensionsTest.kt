ebee876b302afb1201f3c52955f64f06c481c369
Till Hellmund <thellmund@gmail.com>
2019-07-27 19:10:33 +0200

    Add sanity tests for DateExtensions in extension libraries

---
+++ jodatime/src/test/java/com/alamkanak/weekview/jodatime/DateExtensionsTest.kt
@@ -0,0 +1,66 @@
+package com.alamkanak.weekview.jodatime
+
+import org.joda.time.LocalDate
+import org.joda.time.LocalDateTime
+import org.junit.Assert
+import org.junit.Test
+import java.util.Calendar
+
+class DateExtensionsTest {
+
+    @Test
+    fun `toLocalDate() returns correct date`() {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+        }
+        val localDate = LocalDate(1995, 8, 24)
+        Assert.assertEquals(localDate, calendar.toLocalDate())
+    }
+
+    @Test
+    fun `toLocalDateTime() returns correct date and time`() {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+            set(Calendar.HOUR_OF_DAY, 6)
+            set(Calendar.MINUTE, 30)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val localDateTime = LocalDateTime(1995, 8, 24, 6, 30, 0, 0)
+        Assert.assertEquals(localDateTime, calendar.toLocalDateTime())
+    }
+
+    @Test
+    fun `LocalDate toCalendar() returns correct date and time`() {
+        val localDateTime = LocalDateTime(1995, 8, 24, 6, 30, 0, 0)
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+            set(Calendar.HOUR_OF_DAY, 6)
+            set(Calendar.MINUTE, 30)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        Assert.assertEquals(calendar, localDateTime.toCalendar())
+    }
+
+    @Test
+    fun `LocalDateTime toCalendar() returns correct date and time`() {
+        val localDateTime = LocalDateTime(1995, 8, 24, 6, 30, 0, 0)
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+            set(Calendar.HOUR_OF_DAY, 6)
+            set(Calendar.MINUTE, 30)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        Assert.assertEquals(calendar, localDateTime.toCalendar())
+    }
+}
---
+++ threetenabp/src/test/java/com/alamkanak/weekview/threetenabp/DateExtensionsTest.kt
@@ -0,0 +1,67 @@
+package com.alamkanak.weekview.threetenabp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.Month
+import java.util.Calendar
+
+class DateExtensionsTest {
+
+    @Test
+    fun `toLocalDate() returns correct date`() {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+        }
+        val localDate = LocalDate.of(1995, Month.AUGUST, 24)
+        assertEquals(localDate, calendar.toLocalDate())
+    }
+
+    @Test
+    fun `toLocalDateTime() returns correct date and time`() {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+            set(Calendar.HOUR_OF_DAY, 6)
+            set(Calendar.MINUTE, 30)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val localDateTime = LocalDateTime.of(1995, Month.AUGUST, 24, 6, 30, 0, 0)
+        assertEquals(localDateTime, calendar.toLocalDateTime())
+    }
+
+    @Test
+    fun `LocalDate toCalendar() returns correct date and time`() {
+        val localDateTime = LocalDateTime.of(1995, Month.AUGUST, 24, 6, 30, 0, 0)
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+            set(Calendar.HOUR_OF_DAY, 6)
+            set(Calendar.MINUTE, 30)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        assertEquals(calendar, localDateTime.toCalendar())
+    }
+
+    @Test
+    fun `LocalDateTime toCalendar() returns correct date and time`() {
+        val localDateTime = LocalDateTime.of(1995, Month.AUGUST, 24, 6, 30, 0, 0)
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1995)
+            set(Calendar.MONTH, Calendar.AUGUST)
+            set(Calendar.DAY_OF_MONTH, 24)
+            set(Calendar.HOUR_OF_DAY, 6)
+            set(Calendar.MINUTE, 30)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        assertEquals(calendar, localDateTime.toCalendar())
+    }
+}
