f112f6618cd35f9b5535a1be2c2e6e1bf7a43643
Till Hellmund <thellmund@users.noreply.github.com>
2021-03-12 23:01:29 +0100

    Fix issue where text fitting would completely remove subtitle (#241)

--- core/src/main/java/com/alamkanak/weekview/TextFitter.kt
+++ core/src/main/java/com/alamkanak/weekview/TextFitter.kt
@@ -20,7 +20,7 @@ internal class TextFitter(
 
     private fun fitAllDayEvent(eventChip: EventChip): StaticLayout {
         val textPaint = viewState.getTextPaint(eventChip.event)
-        return eventChip.getText().toTextLayout(textPaint, width = Int.MAX_VALUE)
+        return eventChip.getText(includeSubtitle = false).toTextLayout(textPaint, width = Int.MAX_VALUE)
     }
 
     private fun fitSingleEvent(eventChip: EventChip): StaticLayout {
@@ -41,14 +41,6 @@ internal class TextFitter(
             return textLayout
         }
 
-        text = getText(includeSubtitle = false)
-        textLayout = text.toTextLayout(textPaint, width = availableWidth)
-
-        val titleOnlyFits = textLayout.height <= availableHeight
-        if (titleOnlyFits) {
-            return textLayout
-        }
-
         while (textLayout.height > availableHeight && textLayout.lineCount > 1) {
             // Remove the last lines until there's only a single line left. If it doesn't fit
             // by that point, we need to reduce the text size.
@@ -66,7 +58,7 @@ internal class TextFitter(
         return textLayout
     }
 
-    private fun EventChip.getText(includeSubtitle: Boolean = false): CharSequence {
+    private fun EventChip.getText(includeSubtitle: Boolean): CharSequence {
         val subtitle = event.subtitle?.takeIf { event.isNotAllDay && includeSubtitle }
         return combineTitleAndSubtitle(
             title = event.title,
